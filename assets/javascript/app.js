9c1fc3b51f0323f9fa5ef5ef26b68cf657a526a1
Derek Bocskey <bocskey.derek@gmail.com>
2017-12-22 11:51:05 -0500

    more test js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,28 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  console.log("pls");
 
+  //limit to 3 articles per pull
+  var authKey = "f09c13f1ab334133b59ab848df8991cd"
+  var url = "https://api.nytimes.com/svc/mostpopular/v2/mostshared/all-sections/1.json?api-key=" + authKey;
+  //function to tract amount of articles pulled
 
-var numResults = 0;
-var startDate = 0;
-var currentDate = 0;
-//limit to 3 articles per pull
-var articleCounter = 0;
+  //ajax call to pull most shared articles from nytimes
+  $.ajax({
+    url: url,
+    method: 'GET',
+  }).done(function (response) {
+        // Here you work with the response obj from the NYT API
+    var numArt = 3;
+    for (var i = 0; i < numArt; i++) {
+      response.results[i].title;
+      console.log(response.results[i].title);
 
-//function to tract amount of articles pulled
-function runQuery(numArticles, queryURL) {
+    }
 
+    // articleCount = [0, 1, 2]
 
-
-
-
-//ajax call to pull most shared articles from nytimes
-var url = "https://api.nytimes.com/svc/mostpopular/v2/mostshared/all-sections/1.json";
-url += '?' + $.param({
-  'api-key': "f09c13f1ab334133b59ab848df8991cd"
-});
-$.ajax({
-  url: queryURL,
-  method: 'GET',
-}).done(function(result) {
-  console.log(result);
-}).fail(function(err) {
-  throw err;
-});
-
-result();
-
-$("<#articleDiv>").append();
-
-}
+  }).fail(function (err) {
+    throw err;
+  });
 
 });
